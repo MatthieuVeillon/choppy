d6d1c9bfca130caad5e93c64ea57a8f7e726c021
matthieuVeillon <matthieu.veillon@gmail.com>
2019-09-22 20:56:39 -0400

    removed old reducer

--- src/rootReducer/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { recipesReducer } from '../Recipe/reducer/recipe-reducer';
-import { apiFirebaseReducer } from '../apiFirebase/apiFirebase-reducer';
-
-export const rootReducer = combineReducers({
-  recipes: recipesReducer,
-  apiError: apiFirebaseReducer
-});
